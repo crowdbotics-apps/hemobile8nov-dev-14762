0c0ec6c5fa065c56aaf3e80740109f1078f1d37a
Crowdbotics <team@crowdbotics.com>
2020-11-08 05:44:55 +0000

    Initial commit.

---
+++ backend/hemobile8nov_dev_14762/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hemobile8nov_dev_14762 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hemobile8nov_dev_14762.settings')
+
+application = get_wsgi_application()
